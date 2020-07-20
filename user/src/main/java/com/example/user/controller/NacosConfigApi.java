a972d0f67ee39bf336869469bf1978dfe346320c
xinglei <859853145@qq.com>
2020-07-20 19:57:20 +0800

    完善nacos

--- user/src/main/java/com/example/user/controller/NacosConfigApi.java
+++ user/src/main/java/com/example/user/controller/NacosConfigApi.java
@@ -1,10 +1,9 @@
-package com.example.gateway.controller;
+package com.example.user.controller;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-
 
 /**
  * @Desc:
@@ -13,7 +12,6 @@ import org.springframework.web.bind.annotation.RestController;
  * @Copyright: © 找家纺版权所有
  * @Warning: 本内容仅限于公司内部传阅, 禁止外泄或用于其它商业目的
  */
-
 @RefreshScope
 @RestController
 public class NacosConfigApi {
